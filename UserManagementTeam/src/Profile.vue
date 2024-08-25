d427054d7924e10f17e07700efce95a44f45151e
maya-abdoh <mayaabdoh2002@gmail.com>
2024-08-26 02:25:55 +0300

    implement profile component

---
+++ UserManagementTeam/src/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="profile-container">
+      <div class="profile-card">
+        <div class="profile-content">
+          <div class="profile-info">
+            <p><strong>User Name:</strong> {{ profile.name }}</p>
+            <p><strong>Email:</strong> {{ profile.email }}</p>
+            <p><strong>Phone:</strong> {{ profile.phone }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { User } from "./interfaces/ProfileData";
+  
+  const props = defineProps<User>();
+
+  </script>
+  
+
+  
