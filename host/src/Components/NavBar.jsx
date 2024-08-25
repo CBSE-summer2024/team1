3ba4937f1c8d6e72ee6d2fd717c5777079cad80a
noorhonjol <nooralhongal@gmail.com>
2024-08-25 04:26:36 +0300

    change ui library for shell app

--- host/src/Components/NavBar.jsx
+++ host/src/Components/NavBar.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
+
 
 export default function NavBar() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <div className="navbar bg-base-100 shadow">
+            <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
     )
 }
--- host/src/Layout.jsx
+++ host/src/Layout.jsx
@@ -1,16 +1,14 @@
-
-import { Box, Container } from '@mui/material'
 import React from 'react'
 import NavBar from './Components/NavBar'
 import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
     return (
-        <Box>
+        <>
             <NavBar/>
-            <Container>
+            <div className='w-5/6 mx-auto px-12 py-8'>
                 <Outlet/>
-            </Container>
-        </Box>
+            </div>
+        </>
     )
 }
