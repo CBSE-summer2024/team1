2745ee1b7a447e367293f4fc301aed276625dde9
noorhonjol <nooralhongal@gmail.com>
2024-08-27 18:26:48 +0300

    Update UserProfilePage.jsx
    - fix navigation issue

--- host/src/Pages/UserProfilePage.jsx
+++ host/src/Pages/UserProfilePage.jsx
@@ -1,18 +1,15 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 import ComponentAdapter from '../Adapters/ComponentAdapter';
 import { useAuth } from '../context/AuthContext';
 import ProfileComponent from "UserManagementTeam/App"
 export default function UserProfilePage() {
     const session = useAuth();
-    const navigate = useNavigate();
-    if (!session) {
-        navigate('/auth');
-    }
 
     return (
-        session && (
+        session ? (
             <ComponentAdapter framework={'vue'} Component={ProfileComponent} props={session.user} />
-        )
+        ):
+        <Navigate to={'/auth'}/>
     );
 }
