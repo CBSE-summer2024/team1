d5f3e903027d690f1a2d87e42e2bf769edf5b633
noorhonjol <nooralhongal@gmail.com>
2024-08-29 19:23:41 +0300

    fix bug with using top level async with useEffect

--- host/src/Pages/HomePage.jsx
+++ host/src/Pages/HomePage.jsx
@@ -4,22 +4,26 @@ import Header from "DiscoveryTeam/Header";
 import ComponentAdapter from "../Adapters/ComponentAdapter";
 import { supabase } from "../dbConfig";
 
+const images_urls = [
+  "https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ",
+  "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+  "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
+];
 export default function HomePage() {
   const [categories, setCategories] = React.useState([]);
-  const images_urls = [
-    "https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ",
-    "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
-    "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
-  ];
 
-  useEffect(async () => {
-    const { data, error } = await supabase.from("categories").select();
+  useEffect(() => {
+    const getCategories = async()=>{
 
-    if (error) {
-      console.error("Error fetching categories:", error);
-    } else {
-      setCategories(data);
+      const { data, error } = await supabase.from("categories").select();
+   
+      if (error) {
+        console.error("Error fetching categories:", error);
+      } else {
+        setCategories(data);
+      }
     }
+    getCategories()
   }, []);
   return (
     <ComponentAdapter
